8a7fa37323e63d4a66f1fc33241812620d392ef2
MDCDEV <me@mdcdev.me>
2023-04-08 19:50:19 -0500

    312

--- index.js
+++ index.js
@@ -8,7 +8,32 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+function convertDiscordImageURL(url) {
+    // Verificar si la URL tiene la estructura adecuada
+    const regex = /^https:\/\/cdn\.discordapp\.com\/avatars\/\d+\/\w+\.(?:png|jpg|jpeg|gif|webp)$/i;
+    if (!regex.test(url)) {
+      return false;
+    }
+  
+    // Obtener la extensión de archivo de la URL
+    const extension = url.split('.').pop().toLowerCase();
+  
+
+    // Verificar si la extensión es diferente a PNG
+    if (extension !== 'png') {
+      // Reemplazar la extensión en la URL por .png
+      return url.replace(`.${extension}`, '.png');
+    }
+
+  
+    return url; // La URL ya es PNG, no se necesita convertir
+  }
+  
+
 app.get('/', async (req, res) => {
+    if(!req.query.url) return res.send('No url');
+    let url = convertDiscordImageURL(req.query.url);
+    if(!url) return res.send('Not a discord url');
     const canvas = createCanvas(2048, 1173);
     //Get in img the bg.png for bg of the image
     const img = await loadImage(__dirname + '/public/img/bg.png').then(img => {
@@ -18,9 +43,12 @@ app.get('/', async (req, res) => {
     //Get in ctx the context of the canvas
     const ctx = canvas.getContext('2d');
     //Get in img2 the image of the url
-    const downloadedImage = await (await fetch(req.query.url)).buffer();
+    const downloadedImage = await (await fetch(url)).buffer();
     const img2 = await loadImage(downloadedImage).then(img2 => {
         return img2;
+    });
+    const black = await loadImage(__dirname + '/public/img/Black_colour.jpg').then(black => {
+        return black;
     });
     console.log(img2);
     //Set the image in the center of the canvas, with a 200px width and a 200px height, if overpass the canvas, it will be cropped, and make a corner radius of 100px
@@ -34,9 +62,13 @@ app.get('/', async (req, res) => {
     ctx.arc(img.width / 2 - 50, img.height / 2 - 250, 200, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.clip();
+    // Make a bg black for if the image is transparent
+    ctx.drawImage(black, img.width / 2 - 500 / 2, img.height / 2 - 900 / 2, 400, 400);
     ctx.drawImage(img2, img.width / 2 - 500 / 2, img.height / 2 - 900 / 2, 400, 400);
-    //Send the image in view as a png
-    res.send(`<img src="${canvas.toDataURL()}">`);
+    //Show the image in view, withouth saving or using html
+    res.setHeader('Content-Type', 'image/png')
+    res.setHeader('Content-Disposition', 'attachment; filename="image.png"')
+    res.send(canvas.toBuffer());
 });
 
 app.listen(port, () => {
